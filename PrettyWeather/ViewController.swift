8034421a4edf0f8ad7d9fc8cf4a4791345f7eb09
Artie Ford <adf.ford@gmail.com>
2019-12-10 08:16:03 -0500

    Initial Commit

---
+++ PrettyWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PrettyWeather
+//
+//  Created by Arthur Ford on 12/10/19.
+//  Copyright © 2019 Arthur Ford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
